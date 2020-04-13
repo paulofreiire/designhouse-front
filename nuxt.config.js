45a1a28ce9bac6568ae1c27730c897df920e1351
Paulo Freire <paulo.freiire@hotmail.com>
2020-04-13 19:08:23 -0300

    Router config done

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,45 @@
-
 export default {
-  mode: 'spa',
+  mode: 'universal',
   /*
   ** Headers of the page
   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: process.env.npm_package_description || ''}
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+    ],
+    script: [
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.slim.min.js"},
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.0/js/bootstrap.bundle.min.js"},
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"}
     ]
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {color: '#fff'},
   /*
   ** Global CSS
   */
   css: [
+    '@/assets/scss/main.scss'
   ],
   /*
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~plugins/vform'
   ],
   /*
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxtjs/router'
   ],
   /*
   ** Nuxt.js modules
@@ -40,7 +47,27 @@ export default {
   modules: [
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/auth',
+
   ],
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {url: '/login', method: 'post', propertyName: 'token'},
+          logout: {url: '/logout', method: 'post'},
+          user: {url: '/me', method: 'get', propertyName: 'data'}
+        },
+      }
+    }
+  },
+
+  axios: {
+    baseUrl: process.env.API_BASE
+  },
   /*
   ** Build configuration
   */
@@ -48,7 +75,7 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
     }
   }
 }
---
+++ pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      xD
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "about"
+    }
+</script>
+
+<style scoped>
+
+</style>
